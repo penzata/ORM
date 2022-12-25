f32cccd05e142e15db856af711dc3abb06d07b53
penzata <penzata86@yahoo.com>
2022-12-25 17:55:11 +0200

    commit - tests

---
+++ src/test/java/org/example/domain/model/StudentTest.java
@@ -0,0 +1,17 @@
+package org.example.domain.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
