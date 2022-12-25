502e62fcc94c25c7010648bc1d74925382f20bd6
penzata <penzata86@yahoo.com>
2022-12-25 21:48:38 +0200

    commit - annotations tests

--- src/main/java/org/example/persistence/utilities/AnnotationUtils.java
+++ src/main/java/org/example/persistence/utilities/AnnotationUtils.java
@@ -1,8 +1,13 @@
 package org.example.persistence.utilities;
 
+import org.example.persistence.annotations.Entity;
+
 public class AnnotationUtils {
 
+    private AnnotationUtils() {
+    }
+
     public static boolean entityAnnotationIsPresent(Class<?> clss) {
-        return false;
+        return clss.isAnnotationPresent(Entity.class);
     }
 }
--- src/test/java/org/example/persistence/annotations/AnnotationsTest.java
+++ src/test/java/org/example/persistence/annotations/AnnotationsTest.java
@@ -3,22 +3,32 @@ package org.example.persistence.annotations;
 import org.example.persistence.utilities.AnnotationUtils;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class AnnotationsTest {
 
+    @Test
+    void WhenClassMarkedWithEntityAnnotationThenReturnTrue() {
+        boolean result = AnnotationUtils.entityAnnotationIsPresent(TrialClassWithAnnotations.class);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void WhenClassIsNotMarkedWithEntityAnnotationThenReturnFalse() {
+        boolean result = AnnotationUtils.entityAnnotationIsPresent(TrialClassWithoutAnnotations.class);
+
+        assertFalse(result);
+    }
+
     @Entity
-    static class TrialClass {
+    static class TrialClassWithAnnotations {
         Long trialId;
         String trialFirstName;
     }
 
-    @Test
-    void WhenClassMarkedWithEntityAnnotationThenReturnTrue() {
-        boolean result = AnnotationUtils.entityAnnotationIsPresent(TrialClass.class);
-
-        assertTrue(result);
-
+    static class TrialClassWithoutAnnotations {
     }
 }
