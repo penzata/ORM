96a789841d4b85796d8c7c9c057608a338d37bb2
penzata <penzata86@yahoo.com>
2022-12-25 22:53:56 +0200

    commit - refactoring methods to AnnotationUtils

--- src/main/java/org/example/persistence/utilities/AnnotationUtils.java
+++ src/main/java/org/example/persistence/utilities/AnnotationUtils.java
@@ -15,23 +15,14 @@ public class AnnotationUtils {
         return clss.isAnnotationPresent(Entity.class);
     }
 
-    public static String getTableName(Class<?> cls) {
-        Table annotation = cls.getAnnotation(Table.class);
-        if (annotation != null) {
-            String name = annotation.name();
-            if (!name.equals("")) {
-                return name;
-            }
-        }
-        return cls.getSimpleName();
+    public static String getTableName(Class<?> clss) {
+        String tableName = clss.getAnnotation(Table.class).name();
+        return tableName.equals("") ? (clss.getSimpleName() + "s") : tableName;
     }
 
     public static String getFieldName(Field field) {
-        String name = field.getAnnotation(Column.class).name();
-        if (name.equals("")) {
-            name = field.getName();
-        }
-        return name;
+        String fieldName = field.getAnnotation(Column.class).name();
+        return fieldName.equals("") ? field.getName() : fieldName;
     }
 
     public static boolean isUnique(Field field) {
