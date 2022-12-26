e9a735d4ae0784162c9d01faabd3673f8b5c48d3
penzata <penzata86@yahoo.com>
2022-12-26 05:34:25 +0200

    commit - todo

--- src/main/java/org/example/persistence/sql/SQLDialect.java
+++ src/main/java/org/example/persistence/sql/SQLDialect.java
@@ -5,7 +5,7 @@ public class SQLDialect {
     public static final String SQL_INSERT_STUDENT = """
             INSERT INTO STUDENTS (first_name) values(?);
             """;
-
+//todo need to separate type from primary key so that any type can be primary key
     public static final String ID = " BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY";
     public static final String STRING = " VARCHAR(255)";
     public static final String DATETIME = " DATETIME";
