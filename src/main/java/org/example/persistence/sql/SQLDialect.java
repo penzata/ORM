a5f7513fcd25cc10c60f16c20c50df75783c116f
penzata <penzata86@yahoo.com>
2022-12-26 04:51:48 +0200

    commit - added constant

--- src/main/java/org/example/persistence/sql/SQLDialect.java
+++ src/main/java/org/example/persistence/sql/SQLDialect.java
@@ -7,8 +7,9 @@ public class SQLDialect {
             """;
 
     public static final String ID = " BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY";
-    public static final String NAME = " VARCHAR(255) ";
-    public static final String DATETIME = " DATETIME ";
-    public static final String INT = " INT ";
-    public static final String BOOLEAN = " BOOLEAN ";
+    public static final String STRING = " VARCHAR(255)";
+    public static final String DATETIME = " DATETIME";
+    public static final String INT = " INT";
+    public static final String LONG = " BIGINT";
+    public static final String BOOLEAN = " BOOLEAN";
 }
