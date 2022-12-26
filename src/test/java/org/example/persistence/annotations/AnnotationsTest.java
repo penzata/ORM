67bb4fb61c079753155fe286f513977dd77c7567
penzata <penzata86@yahoo.com>
2022-12-26 02:13:38 +0200

    commit - anno tests

--- src/test/java/org/example/persistence/annotations/AnnotationsTest.java
+++ src/test/java/org/example/persistence/annotations/AnnotationsTest.java
@@ -3,6 +3,8 @@ package org.example.persistence.annotations;
 import org.example.persistence.utilities.AnnotationUtils;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -42,27 +44,89 @@ class AnnotationsTest {
 
     @Test
     void WhenTableAnnotationIsPresentAndNameIsDefaultThenReturnClassNameInPlural() {
-        String expectedTableName = "DefaultWithAnnos";
+        String expectedTableName = "DefaultAnnos";
 
-        String tableName = AnnotationUtils.getTableName(DefaultWithAnno.class);
+        String tableName = AnnotationUtils.getTableName(DefaultAnno.class);
 
         assertEquals(expectedTableName, tableName);
+    }
+
+    @Test
+    void WhenColumnAnnotationIsAbsentThenReturnFieldName() {
+        String expectedFieldName = "trialId";
+
+        Field[] declaredFields = WithoutAnno.class.getDeclaredFields();
+        String fieldName = AnnotationUtils.getFieldName(declaredFields[0]);
+
+        assertEquals(expectedFieldName, fieldName);
+    }
+
+    @Test
+    void WhenColumnAnnotationIsPresentAndNameIsNotDefaultThenReturnNameFromAnnotation() {
+        String expectedFieldName = "id";
+
+        Field[] declaredFields = WithAnno.class.getDeclaredFields();
+        String fieldName = AnnotationUtils.getFieldName(declaredFields[0]);
+
+        assertEquals(expectedFieldName, fieldName);
+    }
+
+    @Test
+    void WhenColumnAnnotationIsPresentAndNameIsDefaultThenReturnFieldName() {
+        String expectedTableName = "trialId";
+
+        Field[] declaredFields = DefaultAnno.class.getDeclaredFields();
+        String fieldName = AnnotationUtils.getFieldName(declaredFields[0]);
+
+        assertEquals(expectedTableName, fieldName);
+    }
+
+    @Test
+    void WhenUniqueValueInColumnIsNotPresentThenReturnDefaultOne() throws NoSuchFieldException {
+        boolean expectedUniqueValue = false;
+
+        Field trialId = DefaultAnno.class.getDeclaredField("trialId");
+        boolean uniqueDefault = AnnotationUtils.isUnique(trialId);
+        Field trialId1 = WithAnno.class.getDeclaredField("trialId");
+        boolean uniqueSet = AnnotationUtils.canBeNull(trialId1);
+
+        assertEquals(expectedUniqueValue, uniqueDefault);
+        assertNotEquals(expectedUniqueValue, uniqueSet);
+    }
+
+    @Test
+    void WhenNullableValueInColumnIsNotPresentThenReturnDefaultOne() throws NoSuchFieldException {
+        boolean expectedNullableValue = true;
+
+        Field trialFirstName = DefaultAnno.class.getDeclaredField("trialFirstName");
+        boolean nullableDefault = AnnotationUtils.canBeNull(trialFirstName);
+        Field trialFirstName1 = WithAnno.class.getDeclaredField("trialFirstName");
+        boolean nullableSet = AnnotationUtils.canBeNull(trialFirstName1);
+
+        assertEquals(expectedNullableValue, nullableDefault);
+        assertNotEquals(expectedNullableValue, nullableSet);
     }
 
     @Entity
     @Table(name = "named_table")
     static class WithAnno {
+        @Column(name = "id", unique = true)
         Long trialId;
+        @Column(name = "first_name", nullable = false)
         String trialFirstName;
     }
 
     @Entity
     @Table
-    static class DefaultWithAnno {
+    static class DefaultAnno {
+        @Column
         Long trialId;
+        @Column
         String trialFirstName;
     }
 
     static class WithoutAnno {
+        Long trialId;
+        String trialFirstName;
     }
 }
