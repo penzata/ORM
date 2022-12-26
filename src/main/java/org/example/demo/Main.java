682fe198f107bab576d9d3bfa1744ce4be546ca4
penzata <penzata86@yahoo.com>
2022-12-26 12:04:42 +0200

    commit - deleted exception in signature

--- src/main/java/org/example/demo/Main.java
+++ src/main/java/org/example/demo/Main.java
@@ -4,15 +4,12 @@ import org.example.domain.model.Student;
 import org.example.persistence.ormanager.ORManager;
 import org.example.persistence.utilities.Utils;
 
-import java.sql.SQLException;
-
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         String path = "h2.properties";
         ORManager orManager = Utils.withPropertiesFrom(path);
 
         orManager.register(Student.class);
         orManager.save(new Student("Ivan"));
-
     }
 }
