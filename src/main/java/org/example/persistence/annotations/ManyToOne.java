e48d61bb96e046c8f60fe4d0dfe5a5231ec71bb2
Martin0071 <marto007.1998@gmail.com>
2022-12-27 23:40:19 -0800

    Added the rest of the annotations

---
+++ src/main/java/org/example/persistence/annotations/ManyToOne.java
@@ -0,0 +1,13 @@
+package org.example.persistence.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ManyToOne {
+    Class targetEntity() default void.class;
+    boolean optional() default true;
+}
