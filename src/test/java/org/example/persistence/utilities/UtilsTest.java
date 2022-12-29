a492b176be3625ce12affea6b573bf68fc775c2c
penzata <penzata86@yahoo.com>
2022-12-29 23:41:50 +0200

    commit - withPropertiesFrom tests

--- src/test/java/org/example/persistence/utilities/UtilsTest.java
+++ src/test/java/org/example/persistence/utilities/UtilsTest.java
@@ -3,18 +3,14 @@ package org.example.persistence.utilities;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.db.type.Source;
 import org.assertj.db.type.Table;
-import org.assertj.db.type.ValueType;
 import org.example.domain.model.Student;
 import org.example.persistence.ormanager.ORManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
-import static org.assertj.db.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
@@ -32,7 +28,6 @@ class UtilsTest {
     private static final String SQL_ADD_ONE = "INSERT INTO tests (first_name) VALUES(?)";
     Connection conn;
     PreparedStatement stmt;
-    PreparedStatement ps;
     Source source;
     Table table;
     ORManager ormManager;
@@ -41,6 +36,7 @@ class UtilsTest {
     void setUp() throws SQLException {
         ormManager = Utils.withPropertiesFrom(DATABASE_PATH);
         conn = Utils.getConnection();
+        conn.setAutoCommit(false);
         log.atDebug().log("is the connection valid: {}", conn.isValid(1000));
         conn.prepareStatement(TESTS_TABLE).execute();
         source = new Source("jdbc:h2:file:./src/database/testDB", "", "");
@@ -59,29 +55,17 @@ class UtilsTest {
 
     @Test
     void withPropertiesFromCheck() throws SQLException {
-        try (PreparedStatement stmt = conn.prepareStatement(SQL_ADD_ONE)) {
-            Student st1 = new Student("Bob");
-            Student st2 = new Student("Ani");
-            Student st3 = new Student("Dale");
-            Student st4 = new Student("Laura");
-            Student st5 = new Student("Shelly");
+        try (PreparedStatement stmt = conn.prepareStatement(SQL_ADD_ONE, Statement.RETURN_GENERATED_KEYS)) {
+            Student st1 = new Student("Dick");
 
             stmt.setString(1, st1.getFirstName());
             stmt.executeUpdate();
-            stmt.setString(1, st2.getFirstName());
-            stmt.executeUpdate();
-            stmt.setString(1, st3.getFirstName());
-            stmt.executeUpdate();
-            stmt.setString(1, st4.getFirstName());
-            stmt.executeUpdate();
-            stmt.setString(1, st5.getFirstName());
-            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            while (rs.next()) {
+                st1.setId(rs.getLong(1));
+            }
 
-            assertThat(table).row(0)
-                    .value().isEqualTo(1L)
-                    .value().isEqualTo("Bob");
-
-            assertThat(table).column("first_name").isOfType(ValueType.TEXT, true);
-        }
+            assertThat(st1.getId()).isPositive();
         }
     }
+}
