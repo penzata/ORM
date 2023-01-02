e69db6a56863b88ec3f2b62f9048eb5aacc75ffe
penzata <penzata86@yahoo.com>
2023-01-02 11:57:47 +0200

    commit - exception handling methods

--- src/main/java/org/example/exceptionhandler/ExceptionHandler.java
+++ src/main/java/org/example/exceptionhandler/ExceptionHandler.java
@@ -2,6 +2,7 @@ package org.example.exceptionhandler;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 @Slf4j
@@ -11,23 +12,27 @@ public class ExceptionHandler {
     }
 
     public static void sql(SQLException ex) {
-        log.atError().log("There's some problem with the database access", ex);
+        log.error("There's some problem with the database access", ex);
     }
 
     public static void illegalAccess(ReflectiveOperationException ex) {
-        log.atError().log("Either the underlying field/method is inaccessible" +
+        log.error("Either the underlying field/method is inaccessible" +
                 " or specified object argument is not an instance of the class" +
                 " or interface declaring the underlying field/method", ex);
     }
 
     public static void newInstance(ReflectiveOperationException ex) {
-        log.atError().log("There's some problem initializing a new instance of the constructor's declaring class", ex);
+        log.error("There's some problem initializing a new instance of the constructor's declaring class", ex);
     }
 
     public static void illegalAccessOrNewInstance(ReflectiveOperationException ex) {
-        log.atError().log("Either the underlying field/method is inaccessible" +
+        log.error("Either the underlying field/method is inaccessible" +
                 " or specified object argument is not an instance of the class" +
                 " or interface declaring the underlying field/method " +
                 "or there's some problem initializing a new instance of the constructor's declaring class", ex);
+    }
+
+    public static void inputOutput(IOException ex) {
+        log.error("An error occurred when reading from the input stream.", ex);
     }
 }
