de803ab41667e9d3043f980bb21b446089acf9d4
penzata <penzata86@yahoo.com>
2023-01-04 16:26:10 +0200

    commit - sql dialect generalizing

--- src/main/java/org/example/persistence/sql/SQLDialect.java
+++ src/main/java/org/example/persistence/sql/SQLDialect.java
@@ -8,53 +8,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SQLDialect {
-    public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS";
+    public static final String SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS";
     public static final String SQL_FIND_ALL = """
             SELECT * FROM
             """;
+    public static final String SQL_COUNT_ALL = """
+            SELECT COUNT(*) FROM\040
+            """;
 
-    public static final String ID = " GENERATED ALWAYS AS IDENTITY PRIMARY KEY";
+    public static final String PRIMARY_KEY = " PRIMARY KEY";
     public static final String STRING = " VARCHAR(255)";
-    public static final String DATETIME = " DATETIME";
-    public static final String INT = " INT";
+    public static final String LOCAL_DATE = " DATE";
+    public static final String INTEGER = " INT";
     public static final String LONG = " BIGINT";
+    public static final String DOUBLE = " DOUBLE PRECISION";
     public static final String BOOLEAN = " BOOLEAN";
+    public static final String AUTO_INCREMENT_H2 = " GENERATED ALWAYS AS IDENTITY";
+    public static final String AUTO_INCREMENT_POSTGRE = " SERIAL";
 
     private SQLDialect() {
     }
 
-    public static String getTableAndColumnNamesForInsert(Class<?> clss) {
+    public static String sqlInsertStatement(Class<?> clss) {
         Field[] declaredFields = clss.getDeclaredFields();
         List<String> columnNames = new ArrayList<>();
-        List<String> parameters = new ArrayList<>();
+        List<String> placeholders = new ArrayList<>();
         for (Field declaredField : declaredFields) {
             if (!declaredField.isAnnotationPresent(Id.class)) {
                 columnNames.add(AnnotationUtils.getColumnName(declaredField));
-                parameters.add("?");
+                placeholders.add("?");
             }
         }
         String tableName = AnnotationUtils.getTableName(clss);
 
         return String.format("INSERT INTO %s (%s) values(%s)"
                 , tableName, String.join(", ", columnNames)
-                , String.join(", ", parameters));
+                , String.join(", ", placeholders));
     }
 
-    public static String getTableNameForSelect(Class<?> clss) {
+    public static String sqlSelectStatement(Class<?> clss) {
         String tableName = AnnotationUtils.getTableName(clss);
         return String.format("SELECT * FROM %s WHERE id=?", tableName);
     }
-    public static String getTableAndColumnNamesForUpdate(Class<?> cls){
+
+    public static String sqlUpdateStatement(Class<?> cls) {
         Field[] declaredFields = cls.getDeclaredFields();
-        List<String> columnNames = new ArrayList<>();
-        List<String> parameters = new ArrayList<>();
-        for(Field declaredField: declaredFields) {
+        List<String> columnNamesAndPlaceholders = new ArrayList<>();
+        String placeholder = " = ?";
+        for (Field declaredField : declaredFields) {
             if (!declaredField.isAnnotationPresent(Id.class)) {
-                columnNames.add(AnnotationUtils.getColumnName(declaredField));
-                parameters.add("?");
+                columnNamesAndPlaceholders.add(AnnotationUtils.getColumnName(declaredField) + placeholder);
             }
         }
         String tableName = AnnotationUtils.getTableName(cls);
-        return String.format("UPDATE %s SET %s = (%s)  WHERE id = ?", tableName, String.join(", ", columnNames),String.join(", ", parameters));
+
+        return String.format("UPDATE %s SET %s WHERE id = ?",
+                tableName, String.join(", ", columnNamesAndPlaceholders));
     }
 }
