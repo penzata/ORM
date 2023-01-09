c8a76c604abb819da0e351c037902202694779fe
penzata <penzata86@yahoo.com>
2023-01-09 09:03:34 +0200

    del import

--- src/main/java/org/example/domain/model/Academy.java
+++ src/main/java/org/example/domain/model/Academy.java
@@ -5,7 +5,6 @@ import org.example.persistence.annotations.*;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @Entity
@@ -14,10 +13,10 @@ public class Academy implements Serializable {
     @Id
     @Column(name = "id")
     Long id;
-    @Column(name = "name", nullable = false)
-    private String name;
     @OneToMany(mappedBy = "academies")
     ArrayList<Student> students = new ArrayList<>();
+    @Column(name = "name", nullable = false)
+    private String name;
 
     Academy() {
     }
